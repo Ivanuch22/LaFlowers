204aef6486eed3e3a03f4956e76b482ac5ea60a7
Ivan Kalynych <ivan.kalunuch12@gmail.com>
2021-04-25 22:33:43 +0300

    Create slider

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,3 +3,12 @@ let headerButton = document.querySelector('.header__button');
 headerButton.addEventListener('click', () => {
    menu.classList.toggle('menu--active');
 });
+const swiper = new Swiper('.swiper-container', {
+   navigation: {
+      nextEl: '.swiper-next-arrow',
+      prevEl: '.swiper-prev-arrow',
+   },
+   loop: true,
+   spaceBetween: 17,
+   slidesPerView: 4,
+});

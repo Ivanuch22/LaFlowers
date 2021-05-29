a9176715d7095c0b3fe910272b157cbdf35da302
Ivan Kalynych <ivan.kalunuch12@gmail.com>
2021-05-29 12:10:12 +0300

    Create Corzyna Page

---
+++ docs/js/bag.js
@@ -0,0 +1,42 @@
+const newSiwper = new Swiper('.swiper-container', {
+   navigation: {
+      nextEl: '.swiper-next-arrow',
+      prevEl: '.swiper-prev-arrow',
+   },
+   loop: true,
+   breakpoints: {
+      320: {
+         slidesPerView: 2,
+         spaceBetween: 10,
+      },
+      600: {
+         slidesPerView: 3,
+         spaceBetween: 10,
+      },
+      1024: {
+         spaceBetween: 15,
+         slidesPerView: 3,
+      }
+   }
+});
+
+//
+const amount = document.querySelector('.card__amount');
+const buttonPlus = document.querySelector('.card__price-button--plus');
+const buttonMinus = document.querySelector('.card__price-button--minus');
+const delivery = document.querySelector('.delivery');
+const totalCoast = document.querySelector('.totalCoast');
+const discount = document.querySelector('.discount');
+const coast = document.querySelector('.coast');
+
+
+buttonPlus.addEventListener('click', (a) => {
+   amount.innerHTML = ++amount.innerHTML;
+   totalCoast.innerHTML = ((Number(coast.innerHTML) * Number(amount.innerHTML)) + Number(delivery.innerHTML) - Number(discount.innerHTML))
+});
+buttonMinus.addEventListener('click', (a) => {
+   if (amount.innerHTML > 1) {
+      amount.innerHTML = --amount.innerHTML;
+   }
+   totalCoast.innerHTML = ((Number(coast.innerHTML) * Number(amount.innerHTML)) + Number(delivery.innerHTML) - Number(discount.innerHTML))
+});
